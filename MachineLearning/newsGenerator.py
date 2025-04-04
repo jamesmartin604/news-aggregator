e4c4e1a7d59df04f3df3b05aaa384590b4d413b3
tianlai <tianlai.gu.2024@muemail.ie>
2025-04-04 22:12:46 +0100

    Change to Azure MongoDB.

--- MachineLearning/newsGenerator.py
+++ MachineLearning/newsGenerator.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 import base64
 from openai import AzureOpenAI
 from pymongo import MongoClient
@@ -6,37 +7,35 @@ from pymongo.errors import PyMongoError
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 import pandas as pd
 
-myclient = MongoClient("mongodb://localhost:27017")
-# myclient = MongoClient('localhost',27017)
-print(myclient.list_database_names())
-db = myclient.newsDB
-news_table = db.news
-news_summary_table = db.news_summarize
-news_js = news_table.find({'author': "Joanne Gould"})
-# print(data[0]['title'])
+load_dotenv()
+
+azure_mongo = os.getenv("AZURE_MONGO")
+mongo_client = MongoClient(azure_mongo)
+print(mongo_client.list_database_names())
+db = mongo_client.test
+test_table = db.news
+news_sum = db.news_summarize
+news_js = test_table.find({'author': "Flo Clifford"})
+
+# myclient = MongoClient("mongodb://localhost:27017")
+# # myclient = MongoClient('localhost',27017)
+# print(myclient.list_database_names())
+# db = myclient.newsDB
+# news_table = db.news
+# news_summary_table = db.news_summarize
+# news_js = news_table.find({'author': "Joanne Gould"})
 
 
-
-#endpoint = os.getenv("ENDPOINT_URL", "https://news-summarize-generator.openai.azure.com/")
-# deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4-news-summarize-generator")
-#subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "9Z90r1pd0JGTM3mt2KhFu19OdcV2cgSmkiS0qsjW9n2fvQ23x5nPJQQJ99BCACmepeSXJ3w3AAABACOG5KuG")
-endpoint = os.getenv("ENDPOINT_URL", "https://newsaggregator7983660224.openai.azure.com/")
-deployment = os.getenv("DEPLOYMENT_NAME", "gpt-4o")
-subscription_key = os.getenv("AZURE_OPENAI_API_KEY", "55gPPjfMszfUyfzI0Ty70VFgj4HuzgHHFbvso6RANBBmd9oySIvPJQQJ99BCACYeBjFXJ3w3AAAAACOGCfs7")
+news_gen_endpoint = os.getenv("NEWS_GEN_ENDPOINT")
+news_gen_deployment = os.getenv("NEWS_GEN_DEPLOYMENT_NAME", "gpt-4o")
+news_gen_subscription_key = os.getenv("NEWS_GEN_API_KEY")
 
 # Initialize Azure OpenAI Service client with key-based authentication
 client = AzureOpenAI(
-    azure_endpoint=endpoint,
-    api_key=subscription_key,
+    azure_endpoint=news_gen_endpoint,
+    api_key=news_gen_subscription_key,
     api_version="2024-05-01-preview",
 )
-
-# Initialize Azure OpenAI Service client with key-based authentication    (gpt4)
-# client = AzureOpenAI(
-#     azure_endpoint=endpoint,
-#     api_key=subscription_key,
-#     api_version="2024-05-01-preview",
-# )
 
 
 # IMAGE_PATH = "YOUR_IMAGE_PATH"
@@ -48,12 +47,12 @@ news_content = news_js[0]['title']
 task_news_content = task + news_content
 # chat_prompt = [ {"role": "user", "content": "How are you?"}]
 chat_prompt = [{"role": "user", "content": task_news_content}]
-# Include speech result if speech is enabled  
+# Include speech result if speech is enabled
 messages = chat_prompt
 
 #Generate the completion
 completion = client.chat.completions.create(
-    model=deployment,
+    model=news_gen_deployment,
     messages=messages,
     max_tokens=30, # control the number of the outputs
     temperature=0.2, # 0 <- More stable answer, 1 <- More variety answer
@@ -71,8 +70,11 @@ try:
         "news_title": news_content,
         "summary": summary
     }
-    result = news_summary_table.insert_one(news_summary)
-    print(f"insert success, Doc ID: {result.inserted_id}")
-    print("The number of current document:", news_summary_table.count_documents({}))
+    #result = news_summary_table.insert_one(news_summary)
+    # print(f"insert success, Doc ID: {result.inserted_id}")
+    # print("The number of current document:", news_summary_table.count_documents({}))
+    result = news_sum.insert_one(news_summary)
+    print(f"insert sucess, Doc ID: {result.inserted_id}")
+    print("The number of current document: ", news_sum.count_documents())
 except PyMongoError as e:
     print(f"MongoDB insert failed: {e}")
