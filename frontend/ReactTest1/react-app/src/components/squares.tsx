3df18619cc96fbe3140cfbd95e1424bc30b8a913
Kai <kaithehuman24@gmail.com>
2025-04-06 21:32:27 +0100

    Added large square popup and small dark-mode fix

--- frontend/ReactTest1/react-app/src/components/squares.tsx
+++ frontend/ReactTest1/react-app/src/components/squares.tsx
@@ -1,8 +1,14 @@
 import Square from "./square";
+import {useState} from "react";
+import LargeSquare from "./LargeSquare";//import large square for showing summary
 
 interface Article {
   title: string;
   image: string;
+  description: string;
+  url: string;
+  source: string;
+  published_at: Date;
 }
 
 interface Props {
@@ -11,12 +17,21 @@ interface Props {
 }
 
 function Squares({ articles, handleCategoryChange }: Props) {
+  const[activeSquare, setActiveSquare] = useState<Article | null>(null); //state to cheep track of what square is clicked or not
+
+   const squareClicked = (article: Article) =>{//when square is clicked, open large square
+  setActiveSquare((prev) => ( prev && prev.title === article.title ? null: article)); 
+  }
+
   return (
-    <div className="squares-container">
+    <div className="squares-container"
+      onClick={() => setActiveSquare(null)} //click anywhere not in large square when its active to close large square
+      >
+
       {/* Top One-Third*/}
       <div className="squares-header">
         <h1>
-          AFF<span className="highlight">AI</span>RS
+          <span className="highlight">AI</span>RS
         </h1>
       </div>
 
@@ -49,26 +64,42 @@ function Squares({ articles, handleCategoryChange }: Props) {
         {/* First Row of Squares */}
         <div className="squares-row">
           {articles.slice(0, 4).map((article) => (
+              <div key={article.title} onClick={(e) => { 
+                e.stopPropagation();
+                squareClicked(article); //if square is clicked, open function squareClicked
+              }} >
             <Square
               key={article.title}
               title={article.title}
               imageUrl={article.image}
               grow
             />
+            </div>
           ))}
         </div>
 
         {/* Second Row of Squares */}
         <div className="squares-row">
           {articles.slice(4, 8).map((article) => (
+            <div key={article.title} onClick={(e) => {
+              e.stopPropagation();
+            squareClicked(article); //if square is clicked, open function squareClicked
+            }} >
             <Square
               key={article.title}
               title={article.title}
               imageUrl={article.image}
               grow
             />
+            </div>
           ))}
         </div>
+        {activeSquare &&( //render large square, overlay makes it a popup
+        <div className="overlay" onClick={(e) => e.stopPropagation()}> 
+        <LargeSquare article={activeSquare} onClose={() => setActiveSquare(null)} />
+          </div>
+        )}
+
       </div>
     </div>
   );
