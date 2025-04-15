1126247c9cefb73c5dd9c420c55ee68a0b6255d6
tianlai <tianlai.gu.2024@muemail.ie>
2025-04-15 02:53:11 +0100

    webSeach.py can generate the summary by cronjob

--- MachineLearning/webSearch.py
+++ MachineLearning/webSearch.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
+BATCH_SIZE = 50
 
 load_dotenv()
 
@@ -15,18 +16,45 @@ mongo_client = MongoClient(azure_mongo)
 print(mongo_client.list_database_names())
 db = mongo_client.test
 web_search = db.web_search
+#news = db.news
+news = db.test
+summary_query = {"summary" : { "$exists": False}}
+print(f"news count: {news.count_documents({})}")
+#lack_summarize = news.find(summary_query, no_cursor_timeout=True)
 
-
-payload = json.dumps({
-  "query": "News title is: 'Around 50 firefighters tackle 'severe' house blaze in Tipton', please summary.",
-  "freshness": "noLimit",
-  "summary": True,
-  "count": 5
-})
 headers = {
     'Authorization': web_search_key,
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
-web_search.insert_one(response.json())
+n = BATCH_SIZE
+while n == BATCH_SIZE:
+    n = 0
+    lack_summarize = news.find(summary_query, no_cursor_timeout=True)
+    try:
+        for doc in lack_summarize:
+            n += 1
+            prompt = doc['title']
+            print(f"Processing prompt {n}: {prompt}")
+
+            payload = json.dumps({
+                # "query": "News title is: 'Around 50 firefighters tackle 'severe' house blaze in Tipton', please summary.",
+                "query": prompt,
+                "freshness": "noLimit",
+                "summary": True,
+                "count": 1
+            })
+            response = requests.request("POST", url, headers=headers, data=payload)
+            summary = response.json()["data"]["webPages"]["value"][0]["summary"]
+            print(f"summary: {summary[0: 20]} ...")
+
+            news.update_one({"_id": doc["_id"]},
+                            {"$set": {"summary": summary}})
+
+            if n >= BATCH_SIZE:
+                break
+
+    except Exception as e:
+        print(f"Skip the error: {str(e)}")
+        #n = BATCH_SIZE
+        continue
