c91554978aa28933804032bf4f22198382b12d23
tianlai <tianlai.gu.2024@muemail.ie>
2025-04-20 17:30:01 +0100

    add function application

--- MachineLearning/webSearch.py
+++ MachineLearning/webSearch.py
@@ -1,17 +1,25 @@
 import requests
 import json
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError
 BATCH_SIZE = 50
+# test
 
-load_dotenv()
-
+# load_dotenv()
+NEWS_API_KEY="f9fa966a09b117644338ae57e6ff635d"
+NEWS_API_ENDPOINT="http://api.mediastack.com/v1/news"
+AZURE_MONGO="mongodb+srv://affairs:Online41ver@affairsdb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+NEWS_GEN_ENDPOINT="https://newsaggregator7983660224.openai.azure.com/"
+NEWS_GEN_DEPLOYMENT_NAME="gpt-4o"
+NEWS_GEN_API_KEY="55gPPjfMszfUyfzI0Ty70VFgj4HuzgHHFbvso6RANBBmd9oySIvPJQQJ99BCACYeBjFXJ3w3AAAAACOGCfs7"
+WEB_SEARCH_API_KEY="sk-f788bd3867a8416694affdd159b73a0e"
 url = "https://api.langsearch.com/v1/web-search"
-web_search_key = os.getenv("WEB_SEARCH_API_KEY")
+#url = os.getenv("WEB_SEARCH_API_URL")
+web_search_key = WEB_SEARCH_API_KEY
 
-azure_mongo = os.getenv("AZURE_MONGO")
+azure_mongo = AZURE_MONGO
 mongo_client = MongoClient(azure_mongo)
 print(mongo_client.list_database_names())
 db = mongo_client.test
